39c30ecbf6287ab57ae33a7ec66c4670f78c5ec1
王小磊 <pivstone@gmail.com>
2016-11-01 18:33:28 +0800

    Fix unix test bug 🚀

--- test/docker_test.exs
+++ test/docker_test.exs
@@ -3,45 +3,34 @@ defmodule DockerTest do
   doctest Docker
 
 
-  setup %{} do
-    :meck.new(:gen_tcp, [:unstick])
+  def mock(path) do
+    :meck.new(:gen_tcp, [:unstick,:passthrough])
     :meck.expect(:gen_tcp,:connect,fn(_,_,_) -> {:ok,"socket"} end)
     :meck.expect(:gen_tcp,:send,fn(_,_) -> :ok end)
+    {:ok,pid} = MockServer.start_link(path,"@")
+    :meck.expect(:gen_tcp,:recv,fn(_,_,_)-> {:ok,MockServer.recv(pid)} end)
+    :meck.expect(:gen_tcp,:recv,fn(_,_)-> {:ok,MockServer.recv(pid)} end)
+    :meck.expect(:gen_tcp,:close,fn(_) -> {:ok} end)
+  end
+
+  setup %{} do
     config = Docker.config("http://10.9.8.110:11111")
     {:ok, config: config}
   end
 
   test "GET containers list",%{config: config} do
 
-    {:ok,pid} = MockServer.start_link("test/data/containers/json.data","@")
-    :meck.expect(:gen_tcp,:recv,fn(_,_,_)-> {:ok,MockServer.recv(pid)} end)
-    :meck.expect(:gen_tcp,:recv,fn(_,_)-> {:ok,MockServer.recv(pid)} end)
-    :meck.expect(:gen_tcp,:close,fn(_) -> {:ok} end)
+    mock("test/data/containers/json.data")
     {:ok,resp} = Docker.containers(config)
     assert resp.code==200
-  end
-"""
-  test "Create containers ",%{config: config} do
-    {:ok,resp}  = Docker.create_container(config,%{"Image" => "registry:2"})
-    IO.inspect resp
-    assert resp.code==201
-  end
-
-  test "Start Containers" ,%{config: config} do
-    {:ok,resp} = Docker.start_container(config,"1a5e3c8fdc77")
-    assert resp.code==204
-  end
-
-  test "Stop Containers" ,%{config: config} do
-    {:ok,resp} = Docker.stop_container(config,"1a5e3c8fdc77")
-    assert resp.code==204
+    :meck.unload(:gen_tcp)
   end
 
   test "Inspect Containers" ,%{config: config} do
-    {:ok,resp} = Docker.inspect_container(config,"bef3f3b117e5")
+    mock("test/data/containers/ccb46930869e.data")
+    {:ok,resp} = Docker.container(config,"ccb46930869e")
     assert resp.code==200
+    :meck.unload(:gen_tcp)
   end
-  """
-
 
 end
